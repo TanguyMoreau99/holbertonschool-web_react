aa0b97a30363ed9d2ea756847b13bc6fa4b8184e
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2025-11-26 11:24:40 +0100

    Update task 1: Change CourseList container width to 90%

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -7,7 +7,7 @@ class CourseList extends Component {
     const { courses } = this.props;
 
     return (
-      <div className="w-4/5 mx-auto my-8">
+      <div className="w-[90%] mx-auto my-8">
         <table id="CourseList" className="w-full border-collapse">
           <thead>
             <CourseListRow textFirstCell="Available courses" isHeader={true} />
